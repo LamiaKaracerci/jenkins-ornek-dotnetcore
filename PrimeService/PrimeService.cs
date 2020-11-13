bd89bb5973f86c03cba724e82d403d7c9e928e39
LamiaKaracerci <48009985+LamiaKaracerci@users.noreply.github.com>
2020-11-13 16:06:39 +0300

    Update PrimeService.cs

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -6,8 +6,8 @@ namespace Prime.Services
     {
         public bool IsPrime(int candidate)
         {
+            int kullanilmayan_ve_uzun_bir_isme_sahip_degisken = 1;
             if (candidate < 2)
-                int kullanilmayan_ve_uzun_bir_isme_sahip_degisken = 1;
             {
                 return false;
             }
